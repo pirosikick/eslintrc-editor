b1b2f269d8880e76c308859c3ee8019ebbd79b2a
pirosikick <pirosikick@gmail.com>
2015-10-03 08:49:53 +0900

    Refactor

--- src/components/CheckList.jsx
+++ src/components/CheckList.jsx
@@ -1,6 +1,10 @@
-import React, {Component,  findDOMNode} from "react";
+import {Component,  findDOMNode, PropTypes} from "react";
 import uniqueid from 'uniqueid';
-import {reduce, toArray} from 'lodash';
+import toArray from 'lodash/lang/toArray';
+import clone from 'lodash/lang/clone';
+import remove from 'lodash/array/remove';
+
+const NOOP = function () {};
 
 export default
   class CheckList extends Component {
@@ -12,81 +16,103 @@ export default
       super(props)
 
       this.idPrefix = uniqueid({ prefix: 'checklist' });
-    }
-
-    toggleAll (e) {
-      let {checked} = e.target;
-
-      this.getCheckboxes().forEach((checkbox) => checkbox.checked = checked);
-
-      setTimeout(() => {
-        this.props.onChange(checked ? this.props.keys : []);
-      });
-    }
-
-    onChange(e) {
-      setTimeout(() => {
-        this.props.onChange(this.getAllChecked());
-      });
-    }
-
-    getAllChecked() {
-      return this.getCheckboxes(true).map((cb) => cb.value);
-    }
-
-    getCheckboxes(checked = false) {
-      let selector = `input[type=checkbox]${checked ? ':checked' : ''}`;
-      let tbody = findDOMNode(this.refs.tbody);
-      let checkboxes = tbody.querySelectorAll(selector);
-
-      return toArray(checkboxes);
+      this.onChange = this.onChange.bind(this);
+      this.toggleAll = this.toggleAll.bind(this);
     }
 
     render () {
       let {name, keys, defaultChecked} = this.props;
       let idPrefix = this.idPrefix;
 
+      let rows = keys.map(key =>
+        <TableRow
+          key={`${idPrefix}-${key}`}
+          value={key}
+          onChange={this.onChange}
+          checked={defaultChecked.indexOf(key) !== -1}/>
+      );
+
       return (
         <table className="checklist-table">
           <thead className="checklist-table__header">
             <tr>
               <th>
-                <label htmlFor={`${idPrefix}-toggle-all`}>
+                <label>
                   <input
-                    id={`${idPrefix}-toggle-all`}
                     type="checkbox"
                     className="checkbox__input"
-                    onChange={this.toggleAll.bind(this)}/>
-                  <span className="checkbox__label">name</span>
+                    onChange={this.toggleAll}/>
+                  <span className="checkbox__label">toggle all</span>
                 </label>
               </th>
             </tr>
           </thead>
-          <tbody ref="tbody">{
-            keys.map((key) => (
-              <tr key={`${idPrefix}-${key}`}>
-                <td>
-                  <label htmlFor={`${idPrefix}-${name}-${key}`}>
-                    <input
-                      id={`${idPrefix}-${name}-${key}`}
-                      type="checkbox"
-                      name={name}
-                      value={key}
-                      defaultChecked={
-                        defaultChecked.indexOf(key) >= 0
-                      }
-                      className="checkbox__input"
-                      onChange={this.onChange.bind(this)}/>
-                    <span className="checkbox__label">{key}</span>
-                  </label>
-                </td>
-              </tr>
-            ))
-           }</tbody>
+          <tbody ref="tbody">{rows}</tbody>
         </table>
       );
     }
+
+    toggleAll (e) {
+      let {checked} = e.target;
+      this.props.onChange(checked ? this.props.keys : []);
+    }
+
+    onChange(e) {
+      let {keys, defaultChecked} = this.props;
+      let newChecked = clone(defaultChecked);
+      let {value, checked} = e;
+
+      if (checked) {
+        newChecked.push(value);
+      } else {
+        remove(newChecked, v => v === value);
+      }
+
+      newChecked.sort((a, b) =>
+        keys.indexOf(a) - keys.indexOf(b)
+      );
+
+      this.props.onChange(newChecked);
+    }
   }
 
+class TableRow extends Component {
+  static propTypes = {
+    key: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    defaultChecked: PropTypes.bool
+  };
+  static defaultProps = {
+    key: "",
+    value: "",
+    onChange: NOOP,
+    defaultChecked: false
+  };
 
+  render() {
+    let {key, value, onChange, checked} = this.props;
+
+    return (
+      <tr key={key}>
+        <td>
+          <label>
+            <input
+              type="checkbox"
+              value={value}
+              checked={checked}
+              className="checkbox__input"
+              onChange={this.onChange.bind(this)}/>
+            <span className="checkbox__label">{value}</span>
+          </label>
+        </td>
+      </tr>
+    );
+  }
+
+  onChange(e) {
+    let {checked} = e.target;
+    this.props.onChange({ value: this.props.value, checked });
+  }
+}
 
