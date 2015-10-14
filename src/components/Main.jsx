64dc6a90048090ffefdbdbf71ea3a0c069a04062
pirosikick <pirosikick@gmail.com>
2015-10-14 19:22:27 +0900

    Reset scrollTop when change document

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,14 @@
 import cx from 'classnames';
-import React, {Component} from "react";
+import {Component, findDOMNode} from "react";
 
 export default
   class Main extends Component {
     render () {
       let {className} = this.props;
       return <div {...this.props} className={cx('main', className)}/>;
+    }
+
+    componentDidUpdate() {
+      findDOMNode(this).scrollTop = 0;
     }
   }
