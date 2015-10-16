9edbe456b9d0c27be78ee5b589fdb24dc76ff092
pirosikick <pirosikick@gmail.com>
2015-10-16 09:07:09 +0900

    Refactored

--- src/middlewares/saveState.js
+++ src/middlewares/saveState.js
@@ -3,13 +3,7 @@
 const saveState = function saveState(save) {
   let currentState;
 
-  window.addEventListener('unload', () => {
-    if (!currentState) {
-      return;
-    }
-
-    save(currentState);
-  });
+  window.addEventListener('unload', () => currentState && save(currentState));
 
   return store => next => action => {
     let result = next(action);
