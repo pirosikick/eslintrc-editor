4b8d4940f4e74e7f272f0d0d83861d3ca0cdb0fb
pirosikick <pirosikick@gmail.com>
2015-10-21 22:49:17 +0900

    Added EcmaFeatures.jsx

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,8 @@ import {combineReducers} from 'redux';
 import view from './view';
 import output from './output';
 import globals from './globals';
+import ecmaFeatures from './ecmaFeatures';
 
-export default combineReducers({ view, output, globals });
+export default combineReducers({
+  view, output, globals, ecmaFeatures
+});
